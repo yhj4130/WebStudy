8c8759237b9e1432c9ebf7c011ea7c565c1abdae
yhj4130 <senseheejin@naver.com>
2019-05-08 17:47:13 +0900

    190508

---
+++ WebApp03/WebContent/js/myMenu.js
@@ -0,0 +1,28 @@
+/**
+ * myMenu.js
+ */
+
+function myMenu(status)
+{
+	var menu = document.getElementById("menuTable");
+	
+	if (status==1)
+	{
+		menu.style.display = "block";
+	}
+	else
+	{
+		menu.style.display ="none";
+	}
+}
+
+/*
+function myMenu2()
+{
+	
+}
+function myMenu3()
+{
+	
+}
+*/
